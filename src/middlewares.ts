d4ecd2c3ddd138aafc4921fd3f5a6b7ca8a607a4
kelly <nadjakelly2020@gmail.com>
2023-09-29 20:20:08 -0300

    FIX: atualização dos testes

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,18 +1,17 @@
-import { NextFunction, Request, Response } from "express";
-import market from "./database";
-import Products from "./interfaces";
+import { NextFunction, Request, Response } from "express"
+import Products from "./interfaces"
+import market from "./database"
 
-
-const checkId = (req: Request, res: Response, next: NextFunction) =>{
-    const {id} = req.params
+const checkId = (req: Request, res: Response, next: NextFunction) => {
+    const {id} = req.params; 
     const productsIndex: number = market.findIndex(
-        (v: Products): boolean => v.id === Number(id)
+        (v: Products): boolean =>  v.id === Number(id)
     )
-    if(productsIndex == -1){
+    if(productsIndex === -1){
         return res.status(404).json({message: "Product not found."})
     }
     const foundProducts = market[productsIndex]
-    res.locals = {...res.locals, productsIndex, foundProducts}
+    res.locals = { ...res.locals, productsIndex, foundProducts}
     return next()
 }
 
@@ -22,11 +21,11 @@ const checkName = (req: Request, res: Response, next: NextFunction): void | Resp
     return next()
     const foundProducts: Products | undefined = market.find((
         (p: Products): boolean => p.name === name
-        ))
+    ))
     if(foundProducts){
-        return res.status(409).json({message: "Product already registered"})
+        return res.status(409).json({message: "Product already registered."})
     }
     return next()
 }
 
-export default { checkId, checkName}
+export default { checkId, checkName }
